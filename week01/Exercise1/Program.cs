b27ec5d8f82f71e1d986356b60d53ad9417b2b56
spockledorf <mjarvis1@byupathway.edu>
2026-05-14 11:50:17 -0600

    W1 Excercise 1 complete

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // // This is some sample code to practice some basics.
+        // int x;
+        // x = 7;
+        // Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Console.Write("X = ");
+        // Console.WriteLine(x);
+        // Console.Write("X + 3 = ");
+        // Console.WriteLine(x+3);
+
+        // int y = 5;
+
+        // if (x > y)
+        // {
+        //     Console.Write("X is greater than Y as ");
+        //     Console.Write(x);
+        //     Console.Write(" > ");
+        //     Console.Write(y);
+        // }
+        // if (x < y)
+        // {
+        //     Console.Write("X is less than Y as ");
+        //     Console.Write(x);
+        //     Console.Write(" < ");
+        //     Console.Write(y);
+        // }
+        Console.Write("What is your First Name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your Last Name? ");
+        string lastName = Console.ReadLine();
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
