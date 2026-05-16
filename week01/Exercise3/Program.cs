4cec25ecbf4988ddb4db08f7f03bd05532afa9e8
spockledorf <mjarvis1@byupathway.edu>
2026-05-16 06:50:18 -0600

    W1 Excercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string userInput;
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+
+            Console.WriteLine("Welcome to the Number guessing game!");
+
+            int userGuess;
+            do
+            {
+                // Console.WriteLine($"The magic number: {number}");
+
+                Console.WriteLine("What is your guess?");
+                Console.Write(" > ");
+                userInput = Console.ReadLine();
+                userGuess = int.Parse(userInput);
+                if (userGuess == number)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                else if (userGuess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userGuess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+            } while (userGuess != number);
+            Console.WriteLine("Play again? (yes/no)");
+            Console.Write(" > ");
+            userInput = Console.ReadLine();
+
+        } while (userInput == "yes");
+
     }
 }
