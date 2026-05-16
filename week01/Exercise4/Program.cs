820bdef90713bf1d4cf984b7b2007905ba993078
spockledorf <mjarvis1@byupathway.edu>
2026-05-16 08:58:12 -0600

    W1 Excercise 4 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        double total = 0;
+        int number;
+        do
+        {
+            
+            Console.Write("Enter Number: ");
+            
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+            if (number != 0)
+            {
+                numbers.Add(number);
+                total = total + number;
+            }
+        } while (number != 0);
+
+    double average = total / numbers.Count;
+    int maximum = numbers.Max();
+    
+    // Display Results
+    Console.WriteLine($"Sum: {total}");
+    Console.WriteLine($"Average: {average}");
+    Console.WriteLine($"Largest number: {maximum}");
     }
 }
