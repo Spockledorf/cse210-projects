a1d1f59900af636af000c7a19f9dddb50326af29
spockledorf <mjarvis1@byupathway.edu>
2026-06-11 18:21:58 -0600

    Fixed program to use "shapes" list.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -7,17 +7,17 @@ class Program
         // List of shapes 
         List<Shape> shapes = new List<Shape>();
 
-        Square square1 = new Square("Blue", 8);
-        Rectangle rectangle1 = new Rectangle("Red", 5, 10);
-        Circle circle1 = new Circle("Purple", 4.75);
-        Square square2 = new Square("Crimson", 7);
-        Circle circle2 = new Circle("Yellow", 10);
+        shapes.Add(new Square("Blue", 8));
+        shapes.Add(new Rectangle("Red", 5, 10));
+        shapes.Add(new Circle("Purple", 4.75));
+        shapes.Add(new Square("Crimson", 7));
+        shapes.Add(new Circle("Yellow", 10));
         
         Console.WriteLine(" --- Areas ---");
-        Console.WriteLine($"square1 | Color: {square1.GetColor()} | Area: {square1.GetArea()} sq. u.");
-        Console.WriteLine($"square2 | Color: {square2.GetColor()} | Area: {square2.GetArea()} sq. u.");
-        Console.WriteLine($"rectangle1 | Color: {rectangle1.GetColor()} | Area: {rectangle1.GetArea()} sq. u.");
-        Console.WriteLine($"circle1 | Color: {circle1.GetColor()} | Area: {circle1.GetArea()} sq. u.");
-        Console.WriteLine($"circle2 | Color: {circle2.GetColor()} | Area: {circle2.GetArea()} sq. u.");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Type: {shape.GetType()}");
+            Console.WriteLine($"Color: {shape.GetColor()} | Area: {shape.GetArea()} sq. u.");
+        }
     }
 }
