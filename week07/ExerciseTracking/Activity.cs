9fa2a3f5402232e7cafa6350abd3cc64619b99be
spockledorf <mjarvis1@byupathway.edu>
2026-06-18 17:59:37 -0600

    Cleaned up Activity.cs

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -3,15 +3,35 @@ public abstract class Activity
     private string _dateDay;
     private string _dateMonth;
     private int _dateYear;
-    private int _duration = 0;
+    private int _duration; // Minutes
 
-    public Activity(string day, string month, int year)
+    public Activity(string day, string month, int year, int duration)
     {
         _dateDay = day;
         _dateMonth = month;
         _dateYear = year;
+        _duration = duration;
     }
 
-    public abstract string GetSummary();
+    public int GetDuration()
+    {
+        return _duration;
+    }
+
+    public string GetDate()
+    {
+        return $"{_dateDay} {_dateMonth} {_dateYear}";
+    }
+
     public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{GetDate()} {GetType().Name} ({_duration} min): " +
+               $"Distance {GetDistance():F1} miles, " +
+               $"Speed {GetSpeed():F1} mph, " +
+               $"Pace: {GetPace():F1} min per mile";
+    }
 }
