6b0ffdb8e3dc20c630f14692695f1f6878cc08c9
spockledorf <mjarvis1@byupathway.edu>
2026-06-18 17:56:17 -0600

    Added base methods

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -1,5 +1,17 @@
 public abstract class Activity
 {
-    private DateOnly _date;
+    private string _dateDay;
+    private string _dateMonth;
+    private int _dateYear;
     private int _duration = 0;
+
+    public Activity(string day, string month, int year)
+    {
+        _dateDay = day;
+        _dateMonth = month;
+        _dateYear = year;
+    }
+
+    public abstract string GetSummary();
+    public abstract double GetDistance();
 }
