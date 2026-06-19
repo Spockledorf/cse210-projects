3885dbb74361c85e9c5f909d20fa1aecadfe9217
spockledorf <mjarvis1@byupathway.edu>
2026-06-18 18:05:17 -0600

    Running activity constructor built

--- week07/ExerciseTracking/RunningActivity.cs
+++ week07/ExerciseTracking/RunningActivity.cs
@@ -1,4 +1,12 @@
 public class RunningActivity : Activity
 {
+    private double _distance;
+
+    public RunningActivity(int day, string month, int year, int duration, double distance) : base(day, month, year, duration)
+    {
+        _distance = distance;
+    }
+    
+    
     // TODO
 }
