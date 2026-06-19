126f6b7b79d481a24fbd86b2f557cdc30f4a8689
spockledorf <mjarvis1@byupathway.edu>
2026-06-18 18:06:20 -0600

    Biking activity constructor built

--- week07/ExerciseTracking/BikingActivity.cs
+++ week07/ExerciseTracking/BikingActivity.cs
@@ -1,4 +1,11 @@
 public class BikingActivity : Activity
 {
+    private double _speed;
+
+    public BikingActivity(int day, string month, int year, int duration, double speed) : base(day, month, year, duration)
+    {
+        _speed = speed;
+    }
+    
     // TODO
 }
