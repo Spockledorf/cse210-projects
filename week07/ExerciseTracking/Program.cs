427d3b915dbaed5e3d5b914c6265df86d30a4ef9
spockledorf <mjarvis1@byupathway.edu>
2026-06-18 18:24:57 -0600

    Added more cases of activity

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -7,8 +7,11 @@ class Program
         List<Activity> activities = new List<Activity>
         {
             new RunningActivity(3, "Nov", 2022, 30, 3.0),
-            new BikingActivity(4, "Nov", 2022, 45, 15.0),
-            new SwimmingActivity(5, "Nov", 2022, 40, 40)
+            new RunningActivity(4, "Nov", 2022, 30, 4.8),
+            new BikingActivity(5, "Nov", 2022, 45, 15.0),
+            new BikingActivity(6, "Nov", 2022, 60, 22.4),
+            new SwimmingActivity(7, "Nov", 2022, 30, 40),
+            new SwimmingActivity(8, "Nov", 2022, 40, 45)
         };
 
         foreach (Activity activity in activities)
