3eab6a27eeec510374b9ab746247a8e64f9948a3
spockledorf <mjarvis1@byupathway.edu>
2026-06-18 18:18:40 -0600

    Implemented main program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        List<Activity> activities = new List<Activity>
+        {
+            new RunningActivity(3, "Nov", 2022, 30, 3.0),
+            new BikingActivity(4, "Nov", 2022, 45, 15.0),
+            new SwimmingActivity(5, "Nov", 2022, 40, 40)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
