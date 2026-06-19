312524de0fa95db8e966d8dfde5de0c8c6c57ea6
spockledorf <mjarvis1@byupathway.edu>
2026-06-18 18:07:08 -0600

    Swimming activity constructor built

--- week07/ExerciseTracking/SwimmingActivity.cs
+++ week07/ExerciseTracking/SwimmingActivity.cs
@@ -1,4 +1,11 @@
 public class SwimmingActivity : Activity
 {
+    private int _lapCount;
+
+    public SwimmingActivity(int day, string month, int year, int duration, int lapCount) : base(day, month, year, duration)
+    {
+        _lapCount = lapCount;
+    }
+    
     // TODO
 }
